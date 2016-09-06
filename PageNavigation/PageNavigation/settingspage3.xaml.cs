a6f5d06b1cde209a3de94805a0d5873f25cef28e
Vinaya Swamy <Vinaya.Swamy@ipinfusion.com>
2016-09-06 19:54:36 +0530

    PageNavigation program files are added

---
+++ PageNavigation/PageNavigation/settingspage3.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PageNavigation
+{
+    /// <summary>
+    /// Interaction logic for settingspage3.xaml
+    /// </summary>
+    public partial class settingspage3 : Page
+    {
+        public settingspage3()
+        {
+            InitializeComponent();
+        }
+
+        private void proceed_Click(object sender, RoutedEventArgs e)
+        {
+            settingspage1 st1 = new settingspage1();
+            this.NavigationService.Navigate(st1);
+        }
+    }
+}
